d99f74b56d2ae2bcc27f5f9a0256175644b05348
JotaF <hellsla507@gmail.com>
2025-02-20 19:36:52 -0300

    mudança para o nivel aventureiro

--- CartasTrunfoAventureiro.c
+++ CartasTrunfoAventureiro.c
@@ -19,7 +19,7 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
 
-//declaração de variáveis cidade 1//
+//declaração de variáveis cidade/carta 1 (Estão mal simplificadas pois o github não lê corretamente)
 char estado;
 char codigo[4]; 
 char cidade[40];
@@ -27,6 +27,12 @@ int populacao;
 int turisticos;
 float area;
 float PIB;
+float PIBpercapita;
+float densidadepop;
+
+printf("BEM VINDO AO SISTEMA DE CADASTRO DAS CARTAS DO JOGO SUPERTRUNFO.\n");
+
+printf("Por favor insira as informações corretamente para um melhor funcionamento do programa.\n");
 
 printf("PRIMEIRA CARTA\n");
 
@@ -47,18 +53,21 @@ scanf(" %d", &turisticos);
 //entrada e saída de dados float//
 printf("Area da cidade:\n");
 scanf(" %f", &area);
-printf("PIB:\n");
-scanf(" %f", &PIB);
+printf("digite (xxx.xx bilhoes) para prosseguir\n"
+       "PIB: \n");
+scanf(" %f %*s", &PIB);
 
-//declaração de variáveis cidade 2//
 
+//declaração de variáveis cidade/carta 2 (Estão mal simplificadas pois o github não lê corretamente)
 char Estado;
 char Codigo[4]; 
 char Cidade[40];
 int Populacao;
 int Turisticos;
 float Area;
-float PIBsegundacidade;
+float PIBcarta2;
+float PIBpercapita2;
+float densidadepop2;
 
 printf("SEGUNDA CARTA\n");
 
@@ -79,8 +88,9 @@ scanf(" %d", &Turisticos);
 //entrada e saída de dados float//
 printf("Area da cidade:\n");
 scanf(" %f", &Area);
-printf("PIB:\n");
-scanf(" %f", &PIBsegundacidade);
+printf("digite (xxx.xx bilhoes) para prosseguir\n"
+       "PIB: \n");
+scanf(" %f %*s", &PIBcarta2);
 
 
 //impressao das carta//
@@ -89,18 +99,41 @@ printf("Estado: %c\n", estado);
 printf("Código da carta: %s\n", codigo);
 printf("Nome da cidade: %s\n", cidade);
 printf("População: %d\n", populacao);
-printf("Área :%.2f\n", area);
-printf("PIB: %.1f\n", PIB);
+printf("Área :%.3f km²\n", area);
+printf("PIB: %.2f bilhões de reais\n", PIB);
 printf("Número de pontos turísticos: %d\n", turisticos);
 
-printf("CARTA DA SEGUNDA CIDADE\n");
+
+//Operações  matemáticas//
+PIB = (PIB * 1000000000);
+PIBpercapita = (float) PIB / populacao;//conversão explicita/forçada
+densidadepop = (float) populacao / area; // conversão explicita/forçada 
+
+//impressão das novas adições atualizadas//
+
+printf("Densidade populacional: %.2f hab/km²\n", densidadepop);
+printf("PIB per capita: %.2f reais\n", PIBpercapita);
+
+//impressao das carta da segunda cidade//
+printf("CARTA DA SECUNDA CIDADE\n");
 printf("Estado: %c\n", Estado);
 printf("Código da carta: %s\n", Codigo);
 printf("Nome da cidade: %s\n", Cidade);
 printf("População: %d\n", Populacao);
-printf("Área :%.2f\n", Area);
-printf("PIB: %.1f\n", PIBsegundacidade);
+printf("Área :%.3f km²\n", Area);
+printf("PIB: %.2f bilhões de reais\n", PIBcarta2);
 printf("Número de pontos turísticos: %d\n", Turisticos);
+
+
+//Operações  matemáticas//
+PIBcarta2 = (PIBcarta2 * 1000000000);
+PIBpercapita2 = (float) PIBcarta2 / Populacao;//conversão explicita/forçada
+densidadepop2 = (float) Populacao / Area; // conversão explicita/forçada 
+
+//impressão das novas adições atualizadas//
+
+printf("Densidade populacional: %.2f hab/km²\n", densidadepop2);
+printf("PIB per capita: %.2f reais\n", PIBpercapita2);
 
 return 0;
 }
